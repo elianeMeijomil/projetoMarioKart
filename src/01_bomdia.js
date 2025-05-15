08242210f02d8a41dfb2ed15c8b54c7d0918585a
elianeMeijomil <li.meijomil@gmail.com>
2025-05-15 17:57:31 -0300

    Enunciado da questão 1

--- src/01_bomdia.js
+++ src/01_bomdia.js
@@ -1,3 +1,16 @@
+/*
+Considerando o fluxograma a seguir, assinale a alternativa correta considerando que o usuário dê como entradas os valores 100 e 200, respectivamente.
+*1 ponto
+A imagem mostra um fluxograma que representa um algoritmo. O fluxograma começa com um oval rotulado "início" e termina com um oval rotulado "fim". Entre esses dois pontos, há várias etapas e decisões:
+
+1. Declaração de variáveis: O algoritmo começa com a declaração de duas variáveis inteiras, x e y sendo número inteiros;
+2. Inicialização: A variável i é inicializada com o valor 0.
+3. Loop externo: O algoritmo entra em um loop onde verifica se i é menor que x. Se a condição for verdadeira, ele imprime "bom dia".
+4. Loop interno: Dentro desse loop, há outro loop que inicializa a variável j com o valor 0 e verifica se j é menor que y. Se a condição for verdadeira, ele imprime "boa tarde" e incrementa 1 em j.
+5. Incremento externo: Após sair do loop interno, 1 é incrementado em i.
+6. Impressão final: Quando o loop externo termina, o algoritmo imprime "boa noite".
+
+*/
 let x = 100
 let y = 200
 let qtd_bomdia = 0;
